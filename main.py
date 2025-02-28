982dc1a0379a642e8f06db51104bbb51aaefa37f
rodericaje <roderic.ajorque@hotmail.fr>
2025-02-28 14:53:28 +0100

    Ajout de la fonction calculate md5

--- main.py
+++ main.py
@@ -10,4 +10,12 @@ class File:
         self.date = self.afficher_date_modification()
         self.signature = self.calculate_md5()
         self.first_bytes = self.octets_vers_chaine_hex()
+            
+    def calculate_md5(self, chunk_size=4096):
+        """Calcule la signature MD5 du fichier."""
+        with open(self.path, "rb") as f:
+            hash_md5 = hashlib.md5()
+            for chunk in iter(lambda: f.read(chunk_size), b""):
+                hash_md5.update(chunk)
+            return hash_md5.hexdigest()
 
