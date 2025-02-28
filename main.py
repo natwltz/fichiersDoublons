f684a4d1d7f7f5e7a43e9dbc73aa047984f3321b
rodericaje <roderic.ajorque@hotmail.fr>
2025-02-28 14:40:43 +0100

    Ajout de la classe File

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import hashlib
 import time
 
 class File:
-    def __init__(self, chemin):
-        self.chemin = chemin
-        self.taille = 
-        self.hexa = 
-        self.date =
-        self.signature =
-
+    def __init__(self, path):
+        self.path = path
+        self.name = os.path.basename(path)
+        self.size = os.path.getsize(path)
+        self.date = self.afficher_date_modification()
+        self.signature = self.calculate_md5()
+        self.first_bytes = self.octets_vers_chaine_hex()
 
